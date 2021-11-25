ac34cf7f6aa0df7af4edec89e94fdf9259f64cf3
iron72 <iron72rus@gmail.com>
2021-11-25 16:07:37 +0300

    Correct test

--- src/test/java/ru/netology/StatisticServiceTest.java
+++ src/test/java/ru/netology/StatisticServiceTest.java
@@ -10,8 +10,8 @@ class StatisticServiceTest {
     void findMax() {
         StatisticService service = new StatisticService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
